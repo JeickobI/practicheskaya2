97efc645a8d92f6c288f0c505758d63e79eb9da8
Alexei <Alexeilebedev2006@gmail.com>
2024-04-06 13:35:49 +0400

    Task-02: Added rectangles.cs

---
+++ Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2
+{
+    public class Rectangle
+    {
+        public double Side1 { get; set; }
+        public double Side2 { get; set; }
+
+        public double Perimetr()
+        {
+            return 2 * Side1 + 2 * Side2;
+        }
+
+        public double Ploshad()
+        {
+            return Side1 * Side2;
+        }
+    }
+}
