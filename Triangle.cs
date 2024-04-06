b0a0b562e20d5f329fe711a113a903851a3746e4
Alexei <Alexeilebedev2006@gmail.com>
2024-04-06 13:43:30 +0400

    Task-03: Added triangle.cs

---
+++ Triangle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2
+{
+    public class Triangle
+    {
+        public double a {  get; set; }
+        public double b { get; set; }
+        public double c { get; set; }
+        public double h { get; set; }
+
+        public double S()
+        {
+            return (a * h) / 2;
+        }
+
+        public double P()
+        {
+            return a + b + c;
+        }
+    }
+}
