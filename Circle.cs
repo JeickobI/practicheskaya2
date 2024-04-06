b8d7db0fb4ec26119cf8ffa926288b16268122d4
Alexei <Alexeilebedev2006@gmail.com>
2024-04-06 13:25:48 +0400

    Task-01: Added all files of project

---
+++ Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2
+{
+    public class Circle
+    {
+        public const double PI = Math.PI;
+        public double Radius { get; set; }
+
+        public double Perimetr()
+        {
+            return 2 * PI * Radius;
+        }
+
+        public double Ploshad()
+        {
+            return PI * Radius * Radius;
+        }
+    }
+}
---
+++ Square.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pr2
+{
+    public class Square
+    {
+        public double Side { get; set; }
+
+        public double Perimetr()
+        {
+            return 4 * Side;
+        }
+
+        public double Ploshad()
+        {
+            return Side * Side;
+        }
+    }
+}
